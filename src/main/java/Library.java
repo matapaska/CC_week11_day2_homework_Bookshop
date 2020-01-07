fd19d27167dc14333936e0d76d8e8999c61d8a61
matapaska <marta.pasikowska@gmail.com>
2020-01-07 17:11:22 +0000

    adds addBooks

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -4,11 +4,22 @@ public class Library {
 
     private String name;
     private ArrayList<Book> stock;
+    private int capacity;
 
-    public Library(String name){
+    public Library(String name, int capacity){
         this.name = name;
+        this.capacity = capacity;
         this.stock = new ArrayList<Book>();
     }
 
 
+    public int countBooks() {
+        return this.stock.size();
+    }
+
+    public void addBook(Book book) {
+        if (this.stock.size() < this.capacity) {
+            this.stock.add(book);
+        }
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
 
     private Library library;
@@ -9,12 +11,20 @@ public class LibraryTest {
     @Before
 
     public void before() {
-        library = new Library("Main");
+        library = new Library("Main", 100);
         book = new Book ("Witcher", "Sapkowski", "fantasy");
     }
 
     @Test
-    public void 
+    public void canCountBooks() {
+        assertEquals(0, library.countBooks());
+    }
+
+    @Test
+    public void canAddBook() {
+        library.addBook(book);
+        assertEquals(1, library.countBooks());
+    }
 
 
 }
